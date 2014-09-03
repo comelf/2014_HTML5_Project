9751d5f358b364cf51762e0e63fcb67125022c14
comelf <leebyungwoo@gmail.com>
2014-09-03 11:04:13 +0900

    close #1 #2 #3 #6

--- parallax.js
+++ parallax.js
@@ -1,33 +1,58 @@
 
+var mEvent;
+
 var Parallax = {
 	vWindowHeight : 0,
 	vLastScrollTop : 0,
+	vCurrentTop : 0,
 	init : function(){
+		this.calculatePage(5);
 		window.addEventListener("scroll", this.scrollEvent.bind(this));
 		window.addEventListener("resize",this.windowResizeEvent.bind(this));
+		window.addEventListener("load",this.windowReloadEvent);
 		this.vWindowHeight = window.innerHeight;
 		this.firstPageInitialize();
+
+	},
+
+	calculatePage : function(pageCount){
+		var windowHeight = window.innerHeight;
+		totalHeight = windowHeight * pageCount;
+		var wrapper = document.getElementById("wrapper");
+		wrapper.style.cssText = "height: " + totalHeight + "px;";
 	},
 
 	scrollEvent : function(e) {
-		var top = window.pageYOffset || document.documentElement.scrollTop;
+		clearInterval(mEvent);
+
+		var top  = window.pageYOffset || document.documentElement.scrollTop;
 		var page = parseInt(top / this.vWindowHeight); 
 		var scrollDirection;
+		var duration = 3;
+		var timeCount = 0;
 
-		if (top > this.vLastScrollTop) {
-            this.scrollDown(page);
-        } else {
-            this.scrollUp(page);
-        }
+		
+			(top > this.vLastScrollTop) ?  scrollDirection = "Down" : scrollDirection = "Up";
+			this.vLastScrollTop = top;
 
-        this.vLastScrollTop = top;
+	        mEvent = setInterval(function(){
+	        	//duration 계산
+
+	        	//현재 스크롤 위치와, 움직일 위치 거리 계산
+
+				this["scroll"+scrollDirection](0);
+				(duration<=timeCount) ? clearInterval(mEvent) : timeCount++;
+			}.bind(this),100);
+		
 	},
 
+
 	scrollDown : function(page){
+		console.log("down");
 		switch(page) {
 		    case 0:
 		    case 1:
-		        this.firstParallax();
+		        this.firstScrollDownAction();
 		        break;
 		    case 2:
 		        
@@ -39,7 +64,19 @@ var Parallax = {
 	},
 
 	scrollUp : function(page){
-		var divArr = document.getElementById("firstParallax").childNodes;
+		console.log("up");
+		switch(page) {
+		    case 0:
+		    case 1:
+		        this.firstScrollUpAction();
+		        break;
+		    case 2:
+		        
+		        break;
+		    case 3:
+		        
+		        break;
+		}
 	},
 
 	firstPageInitialize : function(){
@@ -49,7 +86,7 @@ var Parallax = {
 			wWidth  = window.innerWidth; 
 			var top = Math.floor(Math.random() * 10000 % wHeight);
 			var left = Math.floor(Math.random() * 10000 % wWidth);
-			var rate = Math.floor(150 * (Math.random())) +30;
+			var rate = Math.floor(10 * (Math.random()))*10 + 50;
 			var cssStyle = "top: " +top +"px; left: "+ left +"px; height: "+rate+"px; width: "+rate+"px; background-size: "+Math.floor(rate*1.1) +"px;"
 			bubbles[i].style.cssText = cssStyle;
 			bubbles[i].dataset.rate = rate;
@@ -60,13 +97,33 @@ var Parallax = {
 		this.vWindowHeight = e.target.innerHeight;
 	},
 
-	firstParallax : function(){
+	windowReloadEvent : function(e){
+		setTimeout(function(){
+			window.scrollTo(0,0);	
+		},10);
+		
+	},
+
+	firstScrollDownAction : function(){
 		var bubbles = document.querySelectorAll(".bubble");
 		for (var i = 0; i < bubbles.length ; i++) {
 			var rate = 180 - bubbles[i].dataset.rate;
 			var top = bubbles[i].style.top;
 			var topValue = parseInt(top.substring(0,(top.length-2)));
 			var ch = topValue - Math.floor(window.innerHeight * (rate /1200) );
+
+			topValue = ch + "px";
+			bubbles[i].style.top = topValue;
+		}
+	},
+
+	firstScrollUpAction : function(){
+		var bubbles = document.querySelectorAll(".bubble");
+		for (var i = 0; i < bubbles.length ; i++) {
+			var rate = 180 - bubbles[i].dataset.rate;
+			var top = bubbles[i].style.top;
+			var topValue = parseInt(top.substring(0,(top.length-2)));
+			var ch = topValue + Math.floor(window.innerHeight * (rate /1200) );
 
 			topValue = ch + "px";
 			bubbles[i].style.top = topValue;
